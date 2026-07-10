b4ec36efa14fe27ee0d92e4f06616cb53c3f41b4
Ajwad Jahan <azwadjahan77@gmail.com>
2026-07-10 23:37:47 +0530

    added a new method - addFontFromPath(...)

---
+++ src/font/Font.cpp
@@ -0,0 +1,43 @@
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "Font.hpp"
+
+const std::string FontError(const std::string& msg) {
+    return "[FontError]: " + msg;
+}
+
+inline static void ft_check(FT_Error error, const std::string& msg) {
+    if (error) {
+        throw FontError(msg);
+    }
+}
+
+Font::Font() {
+    ft_check(
+        FT_Init_FreeType(&_library),
+        "Failed to initialize Freetype library"
+    );
+}
+
+Font::~Font() {
+    FT_Done_FreeType(_library);
+}
+
+int Font::addFontFromPath(const std::string& path, int font_size) {
+    FT_Face face = nullptr;
+    ft_check(
+        FT_New_Face(_library, path.c_str(), 0, &face),
+        "Failed to create Freetype Face: " + path
+    );
+
+    FT_Set_Pixel_Sizes(face, 0, font_size);
+
+    _FontObject obj = {
+        .face = face,
+        .font_size = font_size
+    };
+
+    cache.insert({cache_counter, obj});
+    return cache_counter++;
+}
--- src/font/Font.hpp
+++ src/font/Font.hpp
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 class Font {
+    FT_Library _library;
 
+    struct _FontObject {
+        FT_Face face;
+        int font_size;
+    };
+
+    int cache_counter = 0;
+    std::unordered_map<int, _FontObject> cache;
+public:
+    Font();
+    ~Font();
+
+    int addFontFromPath(const std::string& path, int font_size);
 };
